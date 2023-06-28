38b72cd47d90880a9d4f025f72a6448e82ff29f6
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-29 09:44:47 +1200

    remove component import from app

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,10 @@
-import Timer from './Timer'
-
 function App() {
   return (
     <>
       <header className="header">
         <h1>My Collection</h1>
       </header>
-      <section className="main">
-        {/* add your code here */}
-        <Timer />
-      </section>
+      <section className="main">{/* add your code here */}</section>
     </>
   )
 }
