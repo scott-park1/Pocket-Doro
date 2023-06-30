a57a534b729399c5f7da1d6fdd1eaa5ab957b4d2
Jen Hurley <j.s.hurley00@gmail.com>
2023-07-01 11:46:42 +1200

    add range inputs for setting working and break lengths

--- client/components/Timer.tsx
+++ client/components/Timer.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEventHandler, ChangeEvent } from 'react'
+import RangeSlider from 'react-range-slider-input'
 import ReactSlider from 'react-slider'
 
 // const audioTune = new Audio('<YOUR_AUDIO_FILE_PATH.mp3>');
@@ -29,10 +30,17 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
   const [longBreakLength, setLongBreakLength] = useState(29)
   const [showSettings, setShowSettings] = useState(false)
 
-  function settings() {
-    setWorkingLength(workingLength)
-    setShortBreakLength(shortBreakLength)
-    setLongBreakLength(longBreakLength)
+  function handleWorkingMinutesChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setWorkingLength(parseInt(value) - 1)
+  }
+  function handleLongBreakChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setLongBreakLength(parseInt(value) - 1)
+  }
+  function handleShortBreakChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    setShortBreakLength(parseInt(value) - 1)
   }
 
   function displaySettings() {
@@ -137,22 +145,39 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
         <button onClick={displaySettings}>
           {showSettings ? 'Close' : 'Settings'}
         </button>
+        <br />
         {showSettings && (
           <>
-            <label>work minutes: {workingLength}:00</label>
-            <ReactSlider
-              value={workingLength}
-              min={1}
-              max={120}
-              onChange={(newValue) => setWorkingLength(newValue)}
-            />
-            <label>short break minutes: {shortBreakLength}:00</label>
-            <ReactSlider
-              value={shortBreakLength}
-              min={1}
-              max={120}
-              onChange={(newValue) => setShortBreakLength(newValue)}
-            />
+            <div>
+              <input
+                type="range"
+                id="working-minutes-settings"
+                name="working-minutes"
+                min="0"
+                max="120"
+                defaultValue={workingLength}
+                step="5"
+                onChange={handleWorkingMinutesChange}
+              ></input>
+              <label htmlFor="short-break-settings">
+                Working Length {workingLength + 1} minutes
+              </label>
+            </div>
+            <div>
+              <input
+                type="range"
+                id="short-break-settings"
+                name="short-break"
+                min="0"
+                max="30"
+                defaultValue={shortBreakLength}
+                step="5"
+                onChange={handleShortBreakChange}
+              ></input>
+              <label htmlFor="short-break-settings">
+                Short Break {shortBreakLength + 1} minutes
+              </label>
+            </div>
             <div>
               <input
                 type="range"
@@ -160,11 +185,12 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
                 name="long-break"
                 min="0"
                 max="120"
-                value={longBreakLength}
-                onChange={settings}
+                defaultValue={longBreakLength}
+                step="5"
+                onChange={handleLongBreakChange}
               ></input>
               <label htmlFor="long-break-settings">
-                Long Break {longBreakLength}
+                Long Break {longBreakLength + 1} minutes
               </label>
             </div>
             <div></div>
