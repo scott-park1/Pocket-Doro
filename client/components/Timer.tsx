5a13a2524aef37ca52433d0628f4a3619a7be18a
Jen Hurley <j.s.hurley00@gmail.com>
2023-06-30 12:31:06 +1200

    fn for play/pause button

--- client/components/Timer.tsx
+++ client/components/Timer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 
 // const audioTune = new Audio('<YOUR_AUDIO_FILE_PATH.mp3>');
 
@@ -17,6 +17,7 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
   const [seconds, setSeconds] = useState(0)
   const [resting, setResting] = useState(false)
   const [completedCycles, setCompletedCycles] = useState(0)
+  const [isPaused, setIsPaused] = useState(false)
 
   // !resting use minutes working
   // resting use resting minutes
@@ -45,6 +46,9 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
     const interval = setInterval(() => {
       // if number of cycles <= 2 short break.
       // if number of cycles > 2 long break, number of cycles = 0
+      if (isPaused) {
+        return
+      }
 
       if (completedCycles <= 2) {
         if (seconds === 0) {
@@ -91,13 +95,20 @@ export default function Timer({ skippedBreaks, onSkipBreak }: Props) {
     onSkipBreak()
   }
 
+  function pauseTimer() {
+    setIsPaused(!isPaused)
+  }
+
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds
 
   return (
     <>
-      <div> Completed work cycles: {completedCycles}</div>
-      <div> breaks skipped: {skippedBreaks}</div> <br />
+      {isPaused ? (
+        <button onClick={pauseTimer}>Play</button>
+      ) : (
+        <button onClick={pauseTimer}>Pause</button>
+      )}
       {resting && (
         <>
           <div>Break time! New session starts in: </div>
