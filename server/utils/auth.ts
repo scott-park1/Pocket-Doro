5bc34ea01e499b4a2995eaab36e26094dbe057f7
Jen Hurley <j.s.hurley00@gmail.com>
2023-07-04 13:24:53 +1200

    typo in auth.ts

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,9 +1,24 @@
 import { auth } from 'express-oauth2-jwt-bearer'
 
+import { expressjwt as jwt, GetVerificationKey } from 'express-jwt'
+import { Request } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { JwtPayload } from 'jsonwebtoken'
+import jwks from 'jwks-rsa'
+
+const domain = 'https://whai-2023-jen.au.auth0.com'
+const audience = 'https://pocket-doro/api'
+
 export const checkJwt = auth({
-  audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_DOMAIN,
-  tokenSigningAlg: 'RS256',
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${domain}/.well-known/jwks.json`,
+  }) as GetVerificationKey,
+  audience: audience,
+  issuer: `${domain}/`,
+  algorithms: ['RS256'],
 })
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
